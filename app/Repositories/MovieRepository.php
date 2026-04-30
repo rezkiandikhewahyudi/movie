4b0230cc012ca602952163a2f6267a660afd5eb9
rezkiandikhewahyudi <rezkiandikhewahyudi@gmail.com>
2026-04-30 11:27:18 +0700

    Update MovieRepository.php

--- app/Repositories/MovieRepository.php
+++ app/Repositories/MovieRepository.php
@@ -9,11 +9,28 @@ class MovieRepository implements MovieRepositoryInterface
 {
     public function getAll()
     {
-        return Movie::all();
+        return Movie::latest()->paginate(10);
+    }
+
+    public function findById($id)
+    {
+        return Movie::findOrFail($id);
     }
 
     public function store($data)
     {
         return Movie::create($data);
+    }
+
+    public function update($id, $data)
+    {
+        $movie = Movie::findOrFail($id);
+        return $movie->update($data);
+    }
+
+    public function delete($id)
+    {
+        $movie = Movie::findOrFail($id);
+        return $movie->delete();
     }
 }

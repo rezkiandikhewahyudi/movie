522294bd8ebf5184b27b290f750210aa8d598c62
rezkiandikhewahyudi <rezkiandikhewahyudi@gmail.com>
2026-04-30 11:28:54 +0700

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
+use App\Repositories\Interfaces\MovieRepositoryInterface;
+use App\Repositories\MovieRepository;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,5 +23,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::useBootstrap();
+    }
+    public function register()
+    {
+    $this->app->bind(MovieRepositoryInterface::class, MovieRepository::class);
     }
 }

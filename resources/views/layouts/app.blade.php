413ec53b33a74d4c6e44c56214a11e585ab339da
rezkiandikhewahyudi <rezkiandikhewahyudi@gmail.com>
2026-04-30 11:33:57 +0700

    Create app.blade.php

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Movie App</title>
+    <meta charset="UTF-8">
+</head>
+<body>
+
+    <h1>🎬 Movie App</h1>
+    <hr>
+
+    @yield('content')
+
+</body>
+</html>
